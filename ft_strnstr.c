48e5945e80e07e0998f7d8290fa40dead157c864
Andre MANDE <anmande@e2r3p14.clusters.42paris.fr>
2022-05-12 14:15:54 +0200

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: anmande <anmande@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 14:57:46 by anmande           #+#    #+#             */
-/*   Updated: 2022/05/12 13:18:34 by anmande          ###   ########.fr       */
+/*   Updated: 2022/05/12 14:14:23 by anmande          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,31 +29,27 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	while (b[i])
 	{
 		j = 0;
-		while (b[i] == l[j] && len > i && b[i])
+		while (b[i + j] == l[j] && b[j] && l[j] && i + j < len)
 		{
+			j++;
 			if (l[j] == '\0')
 			{
-				while (j++ < len)
-					if (b[j] == '\0')
-						return (NULL);
+				return (b + i);
 			}
-			return (b + i);
-			j++;
-			i++;
 		}
-		i -= j;
 		i++;
 	}
 	return (NULL);
 }
 
-
+/*
 int main()
 {
 	const char	*big = "lorem ipsum dolor sit amet";
-	const char	*little = "ipsum";
+	const char	*little = "dolor";
 	size_t	len = 15;
 	printf("%s\n", ft_strnstr(big, little, len));
 	printf("%s\n", strnstr(big, little, len));
 	return (0);
 }
+*/
