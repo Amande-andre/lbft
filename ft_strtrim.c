2768e052537654b85d77c1748196dc61c79734c6
Andre MANDE <anmande@e2r4p13.clusters.42paris.fr>
2022-05-16 12:47:58 +0200

    all

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,24 +6,12 @@
 /*   By: anmande <anmande@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 18:01:50 by anmande           #+#    #+#             */
-/*   Updated: 2022/05/16 12:31:01 by anmande          ###   ########.fr       */
+/*   Updated: 2022/05/16 12:45:52 by anmande          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-/*
-static int	ft_cmp(const char s1, const char *set)
-{
-	size_t	i;
 
-	i = 0;
-	while (set[i] != '\0')
-		if (set[i++] == s1) 
-			return (1);
-	return (0);
-}
-*/
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		i;
@@ -37,20 +25,19 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (ft_strchr(set, s1[j]) && j > i)
 		j--;
 	s1 = (char *)s1 + i;
-	//printf("%i %i ft_calloc(%zu, %i)", j, i, sizeof(char), (j - i) + 1);
 	cpy = ft_calloc(sizeof(char), (j - i) + 1);
 	ft_memcpy(cpy, s1, j - i + 1);
-	//cpy = NULL;
 	return (cpy);
 }
 
-
+/*
 int main()
 {
-	const char * s1 = "	 		";
-	const char *set = " \n\t";
+	const char * s1 = "\t\n32 ";
+	const char *set = "\n \t";
 	//ft_strtrim(s1, set);
 	char* str = ft_strtrim(s1, set);
 	printf("%s\n", str);
 	free(str);
 }
+*/
